7839bfad3ba01f6aee1d1e4c2caa3c4d3b006d1c
Shians <registertonysu@gmail.com>
2018-06-13 11:38:25 +1000

    Changed class to structs and changed private variable naming convention

--- include/gzip_wrappers.h
+++ include/gzip_wrappers.h
@@ -1,6 +1,7 @@
 #pragma once
 #define FMT_HEADER_ONLY
 #include <fmt/format.h>
+#include <experimental/filesystem>
 
 #include <iostream>
 #include <sstream>
@@ -13,43 +14,41 @@
 gzFile gzip_open(const std::string &filename, const std::string &mode);
 void gzip_close(gzFile file);
 
-class GzipOutput {
-    public:
-        GzipOutput(const std::string &filename, const size_t compression_level);
-        GzipOutput(const std::string &filename) : GzipOutput(filename, 4) {};
+struct GzipOutput {
+    GzipOutput(const std::string &filename, const size_t compression_level);
+    GzipOutput(const std::string &filename) : GzipOutput(filename, 4) {};
 
-        void close();
-        void write(const std::string &s);
-        void flush();
-    private:
-        gzFile _fp;
-        std::string _filename;
-        std::queue<std::string> _write_queue;
+    void close();
+    void write(const std::string &s);
+    void flush();
+private:
+    gzFile fp_;
+    std::string filename_;
+    std::queue<std::string> write_queue_;
 };
 
-class OutputPairs {
-    public:
-        OutputPairs(std::vector<std::string> &barcodes, std::string &outdir);
+struct OutputPairs {
+    OutputPairs(std::vector<std::string> &barcodes, std::string &outdir);
 
-        void close_all();
-        void write_file1(const std::string &barcode, const std::string &s);
-        void write_file2(const std::string &barcode, const std::string &s);
+    void close_all();
+    void write_file1(const std::string &barcode, const std::string &s);
+    void write_file2(const std::string &barcode, const std::string &s);
 
-    private:
-        std::vector<GzipOutput> _files1;
-        std::vector<GzipOutput> _files2;
-        std::vector<std::string> _keys;
-        std::string _outdir;
+private:
+    std::vector<GzipOutput> files1_;
+    std::vector<GzipOutput> files2_;
+    std::vector<std::string> keys_;
+    std::string outdir_;
 
-        std::string make_filename(std::string bc, std::string read) {
-            return _outdir + "/" + bc + "_R" + read + ".fastq.gz";
-        }
+    std::string make_filename(std::string bc, std::string read) {
+        return outdir_ + "/" + bc + "_R" + read + ".fastq.gz";
+    }
 
-        std::string make_r1_name(std::string bc) {
-            return make_filename(bc, "1");
-        }
+    std::string make_r1_name(std::string bc) {
+        return make_filename(bc, "1");
+    }
 
-        std::string make_r2_name(std::string bc) {
-            return make_filename(bc, "2");
-        }
+    std::string make_r2_name(std::string bc) {
+        return make_filename(bc, "2");
+    }
 };
