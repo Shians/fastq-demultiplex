7b1a6da181e099e492908ed840ca5427a7f9b0d7
Shians <registertonysu@gmail.com>
2019-09-10 14:22:33 +1000

    Added checking for barcode position

--- include/barcodes.hpp
+++ include/barcodes.hpp
@@ -5,7 +5,12 @@
 #include <sstream>
 #include <fstream>
 
-std::vector<std::string> read_barcodes(std::string bc_file_name) {
+#include "fmt/format.h"
+
+std::vector<std::string> read_barcodes(std::string bc_file_name, int length) {
+    if (length <= 0) {
+        throw std::runtime_error("invalid start and end positions");
+    }
     std::ifstream barcode_file(bc_file_name);
 
     std::vector<std::string> barcodes;
@@ -18,6 +23,9 @@ std::vector<std::string> read_barcodes(std::string bc_file_name) {
         getline(ss, name, ',');
         getline(ss, barcode);
 
+        if (barcode.length() != length) {
+            throw std::runtime_error(fmt::format("invalid barcode: {}", barcode));
+        }
         barcodes.push_back(barcode);
     }
 
